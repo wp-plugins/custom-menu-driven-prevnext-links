fbf53c53224cedb11645d26f4905f7a1994395a7
CodingBee <CodingBee@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-01-17 13:05:48 +0000

    Updated readme file and made css fixes
    git-svn-id: https://plugins.svn.wordpress.org/custom-menu-driven-prevnext-links/trunk@1069985 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- Custom-Menu-Driven-Prev-Next-links.php
+++ Custom-Menu-Driven-Prev-Next-links.php
@@ -1,82 +1,100 @@
-<?php 
-/*
-Plugin name: Custom Menu driven Prev/Next links.
-Description: Shows prev/next links based on custom menu according to custom menu.
-Plugin URI: http://crowdbee.net/wordpress/wordpress-plugins/custom-menu-driven-prev-next-links/
-Version: 0.1
-Author: Susheel Kumar
-License: GPL
-*/
-
-function CMD_prev_next(){ 
-global $post;
-?>
-	<?php $args = array(
-        'order'                  => 'ASC',
-        'orderby'                => 'menu_order',
-        'post_type'              => 'nav_menu_item',
-        'post_status'            => 'publish',
-        'output'                 => 'ARRAY_A',
-        'output_key'             => 'menu_order',
-        'nopaging'               => true,
-        'update_post_term_cache' => false );
-		
-$categories = get_the_category($post->ID); //get the first category as we need only one for links
-$menu =$categories[0]->slug;
-
-	?>
-<?php $menu_items = wp_get_nav_menu_items( $menu, $args );
-if($menu_items){ 
-	foreach($menu_items as $key => $menu_item ) {
-	    $title 		= $menu_item->title;
-	    $url 		= $menu_item->url;
-	    $object_id 	= $menu_item->object_id;
-	    $menu_order = $menu_item->menu_order;
-		if($object_id==$post->ID){ 
-			$order = $menu_item->menu_order-1;//reduce the order by 1 as we have removed the custom menuitem
-			
-				$prev_url 	= $menu_items[$order-1]->url;
-				if($prev_url=='#'){
-					$prev_title	= $menu_items[$order-2]->title;
-					$prev_url 	= $menu_items[$order-2]->url;
-				} else {
-					$prev_url 	= $menu_items[$order-1]->url;
-					$prev_title	= $menu_items[$order-1]->title;
-				}
-			
-			if($order==count($menu_items)){
-				$next_url 	= '';
-				$next_title = '';
-			}else{
-				$next_url 	= $menu_items[$order+1]->url;
-				if($next_url=='#'){
-					$next_url 	= $menu_items[$order+2]->url;
-					$next_title = $menu_items[$order+2]->title;
-				} else {
-					$next_url 	= $menu_items[$order+1]->url;
-					$next_title = $menu_items[$order+1]->title;
-				}
-			}
-		}
-	}
-	
- ?>
-	<ul class="post-nav group sss">
-			<?php if($prev_url!=''){?>
-				<li class="previous"><a href="<?php echo $prev_url;?>" rel="prev"><i class="fa fa-chevron-left"></i><strong>Previous story</strong> <span><?php echo $prev_title;?></span></a>
-				</li>
-			<?php } ?>
-			<?php if($next_url!=''){?>
-				<li class="next"><a href="<?php echo $next_url;?>" rel="next"><i class="fa fa-chevron-right"></i><strong>Next story</strong> <span><?php echo $next_title;?></span></a></li>
-			<?php } ?>
-	 </ul>
-
-<?php } else { ?>
-	
-<ul class="post-nav group">
-	<li class="next"><?php next_post_link('%link', '<i class="fa fa-chevron-right"></i><strong>'.__('Next story', 'hueman').'</strong> <span>%title</span>', true); ?></li>
-	<li class="previous"><?php previous_post_link('%link', '<i class="fa fa-chevron-left"></i><strong>'.__('Previous story', 'hueman').'</strong> <span>%title</span>', true); ?></li>
-</ul>
-<?php } 
-} //function
+<?php 
+/*
+Plugin name: Custom Menu driven Prev/Next links.
+Description: Shows prev/next links based on custom menu according to custom menu.
+Plugin URI: http://crowdbee.net/wordpress/wordpress-plugins/custom-menu-driven-prev-next-links/
+Version: 0.2
+Author: Susheel Kumar
+License: GPL
+*/
+
+function CMD_prev_next($name=''){ 
+global $post;
+?>
+	<?php $args = array(
+        'order'                  => 'ASC',
+        'orderby'                => 'menu_order',
+        'post_type'              => 'nav_menu_item',
+        'post_status'            => 'publish',
+        'output'                 => 'ARRAY_A',
+        'output_key'             => 'menu_order',
+        'nopaging'               => true,
+        'update_post_term_cache' => false );
+		
+$categories = get_the_category($post->ID); //get the first category as we need only one for links
+$menu =$categories[0]->slug;?>
+	
+	
+	
+<?php $menu_items = wp_get_nav_menu_items( $menu, $args );
+if($menu_items){ 
+	foreach($menu_items as $key => $menu_item ) {
+	    $title 		= $menu_item->title;
+	    $url 		= $menu_item->url;
+	    $object_id 	= $menu_item->object_id;
+	    $menu_order = $menu_item->menu_order;
+		if($object_id==$post->ID){ 
+			$order = $menu_item->menu_order-1;//reduce the order by 1 as we have removed the custom menuitem
+			
+				$prev_url 	= $menu_items[$order-1]->url;
+				if($prev_url=='#'){
+					$prev_title	= $menu_items[$order-2]->title;
+					$prev_url 	= $menu_items[$order-2]->url;
+				} else {
+					$prev_url 	= $menu_items[$order-1]->url;
+					$prev_title	= $menu_items[$order-1]->title;
+				}
+			
+			if($order==count($menu_items)){
+				$next_url 	= '';
+				$next_title = '';
+			}else{
+				$next_url 	= $menu_items[$order+1]->url;
+				if($next_url=='#'){
+					$next_url 	= $menu_items[$order+2]->url;
+					$next_title = $menu_items[$order+2]->title;
+				} else {
+					$next_url 	= $menu_items[$order+1]->url;
+					$next_title = $menu_items[$order+1]->title;
+				}
+			}
+		}
+	}
+	
+ ?>
+	<ul class="cmd_post_nav">
+			<?php if($prev_url!=''){?>
+				<li class="cmd_previous">
+					<a href="<?php echo $prev_url;?>" rel="prev">
+						<span class="meta-nav">&lt;</span>
+						<div class="cmd_left">
+							<strong>Previous <?php echo $name;?></strong><br> 
+							<span><?php echo $prev_title;?></span>
+						</div>
+					</a>
+				</li>
+			<?php } ?>
+			<?php if($next_url!=''){?>
+				<li class="cmd_next">
+					<a href="<?php echo $next_url;?>" rel="next">
+						<span class="meta-nav">&gt;</span>
+						<div class="cmd_right">
+							<strong>Next <?php echo $name;?></strong> <br>
+							<span><?php echo $next_title;?></span> 
+						</div>
+						
+					</a>
+				</li>
+			<?php } ?>
+	 </ul>
+
+<?php } else { ?>
+	
+<ul class="cmd_post_nav group">
+	<li class="cmd_next">
+		<?php next_post_link('%link', '<span class="meta-nav">&gt;</span><div class="cmd_right"><strong>'.__('Next Post', 'hueman').'</strong> <br><span>%title</span></div>', true); ?></li>
+	<li class="cmd_previous"><?php previous_post_link('%link', '<span class="meta-nav">&lt;</span><div class="cmd_left"><strong>'.__('Previous Post', 'hueman').'</strong> <br><span>%title</span></div>', true); ?></li>
+</ul>
+<?php } 
+} //function
 ?>
--- readme.txt
+++ readme.txt
@@ -1,38 +1,24 @@
 === Custom Menu Driven Prev/Next Links ===
-Contributors: Susheel Kumar, CodingBee
+Contributors: CodingBee, Susheel Kumar
 Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=3MX23YDX9XU3Y
 Tags: prev/next, prev-next, previus & next links, custom menu
 License: GPLv2 or later
 Requires at least: 4.0.0
 Tested up to: 4.0.1
-Stable tag: 0.1
+Stable tag: 0.2
 
 This plugin lets you display prev/next links at the bottom of your posts, and they are dictated by a custom menu.   
 
 == Description ==
 
-If you have a collection of posts that you want your readers to view in a particular sequence then this plugin can help. 
+If you have a collection of posts that you want your readers to view in a particular sequence then this plugin can help, because it places previous/next links at the bottom of each post.  
 
-Let's take a look at an example, if you check out the screenshots section, you'll find a screenshot taken from the w3school's website, in particular a it is a post about "PHP constants", you'll notice that there is a menu called "PHP Tutorials" on the left hand side and that the post we are viewing is about "constants" which appears about halfway down this menu. According this menu, the 2 posts on either side, are about "PHP Strings" and "PHP Operators". You can access these adjacent posts by clicking the menu links. Alternatively if you scroll down this post, you will find "Previous" and "Next" links that you can use instead. This makes it easier for your readers to jump to the previous/next post once they have reached the bottom without needing to scroll back up to the menu again. 
 
-This plugin lets you replicate this feature by generating the prev/next links which appears at the bottom of each post.
 
 
 = Usage Instructions =
 
-At the moment, this plugin is very basic and doesn't introduce any additional options/settings on the backend, so don't bother looking for them! After you have installed this plugin, do the following:
-
-1. Group your posts by placing them into the same category.
-2. Then go to Appearance -> Menus
-3. Create a new custom menu. This custom menu's name must match the category's name. 
-4. Now add your group of posts to the menu and order them to your liking. Note, if you can't find the option to add posts to the custom menu, then try going to "screen options", which is at the top right, and then enable the "posts" checkbox. 
-5. Now go to Appearance -> Widget
-6. Add your newly created custom menu to your Sidebar. 
-7. Optional step: One thing you may want to do is only display this custom menu on certain posts only, i.e. only display this menu on posts that belongs to a certain category. You can achieve this by using the very hand [Display Widgets](https://wordpress.org/plugins/display-widgets "Display Widgets") plugin.
-8. Now comes the tricky part, go to Appearance -> Editor. Here you need to insert "<?php CMD_prev_next();?>" into one of the php files (which are listed on the far right). Exactly which file you need to edit depends on the particular Wordpress theme that you're using.  A good starting point could be going to a file called "single.php".       
-
-Once you are done, go to one of the posts that are in the custom menu and see whether you can view see the custom menu along with the previous/next links. 
-
+Please check our [guide](http://www.codingbee.net/tutorials/wordpress/wordpress-set-up-a-navigation-structure-like-w3schools-and-tutorialspoint/ "CodingBee") on how to use this plugin. 
 
 = New Features =
 If you would like a new feature to be added, then please let us know via the support forum. 
@@ -62,6 +48,10 @@ If you have a series of posts that you want your audience to view in a certain o
   
   
 == Changelog ==
+
+= 0.1 =
+* 17/01/15
+* Updated Readme file and made some css fixes.
 
 
 = 0.1 =
